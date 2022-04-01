c9045170fcd352b88b7794d7fdd727a67b36620a
Maximilian Hausdorf <maxhausdorf@iphonimilian.home>
2022-04-01 22:00:33 +0200

    Added LobbyControllerTest and wrote a test for the create new lobby method #4

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/controller/LobbyControllerTest.java
@@ -0,0 +1,89 @@
+package ch.uzh.ifi.hase.soprafs22.controller;
+
+import ch.uzh.ifi.hase.soprafs22.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs22.rest.dto.LobbyPostDTO;
+import ch.uzh.ifi.hase.soprafs22.service.LobbyService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.web.server.ResponseStatusException;
+
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * LobbyControllerTest
+ * This is a WebMvcTest which allows to test the UserController i.e. GET/POST
+ * request without actually sending them over the network.
+ * This tests if the UserController works.
+ */
+@WebMvcTest(LobbyController.class)
+public class LobbyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private LobbyService lobbyService;
+
+
+    // create a new user
+    @Test
+    public void createUser_validInput_userCreated() throws Exception {
+        // given
+        Lobby lobby = new Lobby();
+        lobby.setHostId(7L);
+        lobby.setLobbyName("ToxicMW2Lobby");
+        lobby.setisPublic(false);
+        lobby.setisPrivate(true);
+
+        LobbyPostDTO lobbyPostDTO = new LobbyPostDTO();
+        lobbyPostDTO.setHostId(7L);
+        lobbyPostDTO.setLobbyName("ToxicMW2Lobby");
+        lobbyPostDTO.setisPrivate(true);
+        lobbyPostDTO.setisPublic(false);
+
+        given(lobbyService.createLobby(Mockito.any())).willReturn(lobby);
+
+        // when/then -> do the request + validate the result
+        MockHttpServletRequestBuilder postRequest = post("/lobbies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(lobbyPostDTO));
+
+        // then
+        mockMvc.perform(postRequest)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.hostId", is(lobby.getHostId().intValue())))
+                .andExpect(jsonPath("$.lobbyName", is(lobby.getLobbyName())))
+                .andExpect(jsonPath("$.isPublic", is(lobby.getisPublic())));
+    }
+
+    /**
+     * Helper Method to convert userPostDTO into a JSON string such that the input
+     * can be processed
+     * Input will look like this: {"username": "testUsername"}
+     *
+     * @param object
+     * @return string
+     */
+    private String asJsonString(final Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    String.format("The request body could not be created.%s", e.toString()));
+        }
+    }
+}
