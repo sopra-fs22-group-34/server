e067af1b3a57c52e70bb92c51ad24016fbd0eb7d
SamuelBrander <samuelbrander99@gmail.com>
2022-04-26 14:19:22 +0200

    Implemented checkIfMoveValid() and executeMove() in Middle, added test cases, #49 and #50

--- src/main/java/ch/uzh/ifi/hase/soprafs22/entity/Middle.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/entity/Middle.java
@@ -19,4 +19,38 @@ public class Middle implements Field {
         }
 
     }
+
+    public boolean checkIfMoveValid(Move attemptedMove) {
+        int colorIndex = attemptedMove.getColorIndex();
+        int tileAmount = attemptedMove.getTileAmount();
+
+        if (colorAmounts[colorIndex] == tileAmount) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+    //executeMove returns whether the minus Tile has been picked up during the move, since the first player
+    //to take tiles from the Middle must also take the minus Tile
+    public boolean executeMove(Move move) {
+        colorAmounts[move.getColorIndex()] = 0;
+
+        if (hasMinusTile) {
+            hasMinusTile = false;
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+    public void placeLeftoverTiles(Integer[] leftoverColorAmounts) {
+        for (int i = 0; i < 5; i++) {
+            colorAmounts[i] += leftoverColorAmounts[i];
+        }
+    }
 }
--- src/test/java/ch/uzh/ifi/hase/soprafs22/entity/MiddleTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/entity/MiddleTest.java
@@ -17,4 +17,58 @@ public class MiddleTest {
 
         assertEquals(0, sum);
     }
+
+    @Test
+    public void leftoverTilePlacement() {
+        Middle testMiddle = new Middle();
+        Integer[] leftoverTiles = {0,1,1,0,0};
+
+        //ensure leftover tiles are placed correctly
+        testMiddle.placeLeftoverTiles(leftoverTiles);
+        assertArrayEquals(testMiddle.getColorAmounts(), leftoverTiles);
+    }
+
+    @Test
+    public void checkValidMove() {
+        Middle testMiddle = new Middle();
+
+        Integer[] leftoverTiles = {0,2,1,0,0};
+        testMiddle.placeLeftoverTiles(leftoverTiles);
+
+
+        Move testMove = new Move(0,1,0,2,0);
+        assertTrue(testMiddle.checkIfMoveValid(testMove));
+    }
+
+    @Test
+    public void checkInvalidMoveTooManyTiles() {
+        Middle testMiddle = new Middle();
+
+        Integer[] leftoverTiles = {0,1,1,0,0};
+        testMiddle.placeLeftoverTiles(leftoverTiles);
+
+
+        Move testMove = new Move(0,1,0,2,0);
+        assertFalse(testMiddle.checkIfMoveValid(testMove));
+    }
+
+    @Test
+    public void executeMoveTest() {
+        Middle testMiddle = new Middle();
+
+        Integer[] leftoverTiles = {0,2,1,0,0};
+        testMiddle.placeLeftoverTiles(leftoverTiles);
+
+        //check if the first move correctly returns that the minus tile is to be picked up
+        Move testMoveOne = new Move(0,1,0,2,0);
+        assertTrue(testMiddle.executeMove(testMoveOne));
+
+        Integer[] expectedColorAmounts = {0,0,1,0,0};
+
+        assertArrayEquals(expectedColorAmounts, testMiddle.getColorAmounts());
+
+        //check if the first move correctly returns that the minus tile is to be picked up
+        Move testMoveTwo = new Move(0,2,0,1,0);
+        assertFalse(testMiddle.executeMove(testMoveTwo));
+    }
 }
