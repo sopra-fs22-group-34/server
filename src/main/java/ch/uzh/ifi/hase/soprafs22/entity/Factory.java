5f587826d7b90b87b887fb5cc7dd06f851f69f9e
SamuelBrander <samuelbrander99@gmail.com>
2022-04-26 13:51:34 +0200

    Implemented checkIfMoveValid() and executeMove() in Factory, added test cases, #49 and #50

--- src/main/java/ch/uzh/ifi/hase/soprafs22/entity/Factory.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/entity/Factory.java
@@ -13,13 +13,42 @@ public class Factory implements Field {
 
     Factory() {
         Random random = new Random();
+
+        //initialize colorAmounts to all zero
         for (int i = 0; i < 5; i++) {
             colorAmounts[i] = 0;
         }
 
+        //generate four tiles of random color
         for (int i = 0; i < 4; i++) {
             int randomInt = random.nextInt(5);
             colorAmounts[randomInt]++;
         }
+    }
+
+    public boolean checkIfMoveValid(Move attemptedMove) {
+        int colorIndex = attemptedMove.getColorIndex();
+        int tileAmount = attemptedMove.getTileAmount();
+
+        if (colorAmounts[colorIndex] == tileAmount) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+
+    //this needs to return an Integer array to reflect the leftover tiles that need to be placed in the middle
+    public Integer[] executeMove(Move move) {
+        Integer[] leftoverColorAmounts = colorAmounts;
+        leftoverColorAmounts[move.getColorIndex()] = 0;
+
+        for (int i = 0; i < 5; i++) {
+            colorAmounts[i] = 0;
+        }
+        //set all colorAmounts to 0, since Factory now empty, and return leftover tiles
+        return leftoverColorAmounts;
     }
 }
--- src/test/java/ch/uzh/ifi/hase/soprafs22/entity/FactoryTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/entity/FactoryTest.java
@@ -17,4 +17,79 @@ public class FactoryTest {
 
         assertEquals(4, sum);
     }
+
+    @Test
+    public void checkValidMove() {
+        Factory testFactory = new Factory();
+        Integer[] colorAmounts = testFactory.getColorAmounts();
+
+        int colorIndex = 0;
+        int tileAmount = 0;
+
+        //find first nonzero entry in colorAmounts, create Move with this colorIndex and tileAmount
+        for (int i = 0; i < 5; i++) {
+            if (colorAmounts[i] != 0) {
+                colorIndex = i;
+                tileAmount = colorAmounts[i];
+                break;
+            }
+        }
+
+        Move testMove = new Move(0,colorIndex,0,tileAmount,0);
+        assertTrue(testFactory.checkIfMoveValid(testMove));
+    }
+
+    @Test
+    public void checkInvalidMoveTooManyTiles() {
+        Factory testFactory = new Factory();
+        Integer[] colorAmounts = testFactory.getColorAmounts();
+
+        int colorIndex = 0;
+        int tileAmount = 0;
+
+        //find first nonzero entry in colorAmounts, create Move with this colorIndex and tileAmount
+        for (int i = 0; i < 5; i++) {
+            if (colorAmounts[i] != 0) {
+                colorIndex = i;
+                tileAmount = colorAmounts[i] + 1;
+                break;
+            }
+        }
+
+        Move testMove = new Move(0,colorIndex,0,tileAmount,0);
+        assertFalse(testFactory.checkIfMoveValid(testMove));
+    }
+
+    @Test
+    public void executeMoveTest() {
+        Factory testFactory = new Factory();
+        Integer[] colorAmounts = testFactory.getColorAmounts();
+
+        int colorIndex = 0;
+        int tileAmount = 0;
+
+        //find first nonzero entry in colorAmounts, create Move with this colorIndex and tileAmount
+        for (int i = 0; i < 5; i++) {
+            if (colorAmounts[i] != 0) {
+                colorIndex = i;
+                tileAmount = colorAmounts[i];
+                break;
+            }
+        }
+
+        Move testMove = new Move(0,colorIndex,0,tileAmount,0);
+
+
+        //create arrays for expected returned colorAmounts and expected colorAmounts in the Factory
+        Integer[] expectedLeftoverColorAmounts = colorAmounts;
+        expectedLeftoverColorAmounts[colorIndex] = 0;
+
+        Integer[] emptyFactoryColorAmounts = new Integer[5];
+        for (int i = 0; i < 5; i++) {
+            emptyFactoryColorAmounts[i] = 0;
+        }
+
+        assertArrayEquals(expectedLeftoverColorAmounts, testFactory.executeMove(testMove));
+        assertArrayEquals(emptyFactoryColorAmounts, testFactory.getColorAmounts());
+    }
 }
