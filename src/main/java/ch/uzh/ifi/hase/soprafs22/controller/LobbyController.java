89708013fd252b0809ad4a78d98574fb41fbfa59
maxhausdorf <maxhausdorf@macbook-pro-von-maximilian.home>
2022-04-30 19:54:42 +0200

    It is now possible to make moves.

--- src/main/java/ch/uzh/ifi/hase/soprafs22/controller/LobbyController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/controller/LobbyController.java
@@ -147,29 +147,14 @@ public class LobbyController {
     @PutMapping("/lobbies/{lobbyId}/game/moves")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @ResponseBody
-    public void executeGameMove(@PathVariable long lobbyId, @RequestBody JSONObject moveJson) {
-        int originIndex = moveJson.getInt("originIndex");
-        int colorIndex = moveJson.getInt("colorIndex");
-        int targetRowIndex = moveJson.getInt("targetRowIndex");
-        int tileAmount = moveJson.getInt("tileAmount");
-        int playerIndex = moveJson.getInt("playerIndex");
-
-        Move move = new Move(originIndex,colorIndex,targetRowIndex,tileAmount,playerIndex);
+    public void executeGameMove(@PathVariable long lobbyId, @RequestBody Move move) {
         lobbyService.executeMove(move, lobbyId);
-
     }
 
     @GetMapping("/lobbies/{lobbyId}/game/moves")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public void checkIfMoveValid(@PathVariable long lobbyId, @RequestBody JSONObject attemptedMoveJson) {
-        int originIndex = attemptedMoveJson.getInt("originIndex");
-        int colorIndex = attemptedMoveJson.getInt("colorIndex");
-        int targetRowIndex = attemptedMoveJson.getInt("targetRowIndex");
-        int tileAmount = attemptedMoveJson.getInt("tileAmount");
-        int playerIndex = attemptedMoveJson.getInt("playerIndex");
-
-        Move attemptedMove = new Move(originIndex,colorIndex,targetRowIndex,tileAmount,playerIndex);
-        lobbyService.executeMove(attemptedMove, lobbyId);
+    public boolean checkIfMoveValid(@PathVariable long lobbyId, @RequestBody Move attemptedMove) {
+        return lobbyService.checkIfMoveValid(attemptedMove, lobbyId);
     }
 }
